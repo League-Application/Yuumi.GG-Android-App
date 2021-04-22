c390e9f948cbdcea38c467c43b6e400cf6234b16
Kareem100 <github.com/Kareemhegazy133>
2021-04-21 22:34:06 -0400

    Workig code for now!

--- app/src/main/java/com/LeagueApplication/YummiGG/MainActivity.java
+++ app/src/main/java/com/LeagueApplication/YummiGG/MainActivity.java
@@ -1,7 +1,6 @@
 package com.LeagueApplication.YummiGG;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-
 import org.parceler.Parcels;
-
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class MainActivity extends AppCompatActivity {
 
